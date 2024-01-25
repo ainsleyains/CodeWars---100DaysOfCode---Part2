d2dafceaf9a9da65b265c37880407c0fcc511157
Ainsley <ainsleyjyl@gmail.com>
2024-01-25 14:41:36 -0800

    Price is right

---
+++ Price is right.js
@@ -0,0 +1,11 @@
+/*
+Given an array of integers, and a target integer, return the largest integer from the array that is less than or equal to the target.
+
+If there is no number that is less than or equal to the target number then return undefined.
+
+The array of integers may be empty, but will always be an array.
+*/
+
+function priceIsRight(numbers, target) {
+    return numbers.sort((a, b) => b - a).find((el) => el <= target);
+}
